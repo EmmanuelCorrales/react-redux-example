0e7d0934dc8c75c5a258a3d927bbaea960dd099a
Emmanuel Corrales <emmanuelcorrales22@gmail.com>
2017-08-08 10:37:14 +0800

    Install necessary react libraries.

--- public/bundle.js
+++ public/bundle.js
@@ -65,10 +65,9 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-"use strict";
-
+throw new Error("Module build failed: SyntaxError: Unexpected token (4:2)\n\n\u001b[0m \u001b[90m 2 | \u001b[39m\n \u001b[90m 3 | \u001b[39mrender({\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 4 | \u001b[39m  \u001b[33m<\u001b[39m\u001b[33mh1\u001b[39m\u001b[33m>\u001b[39m\u001b[33mHello\u001b[39m\u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mh1\u001b[39m\u001b[33m>\u001b[39m\n \u001b[90m   | \u001b[39m  \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 5 | \u001b[39m})\u001b[33m;\u001b[39m\n \u001b[90m 6 | \u001b[39m\u001b[0m\n");
 
 /***/ })
 /******/ ]);
