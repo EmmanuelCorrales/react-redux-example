7ea643aaddfe7794cecc57d7f478ddd87133fe62
Emmanuel Corrales <emmanuelcorrales22@gmail.com>
2017-08-11 12:32:08 +0800

    Run app on port 8080.

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+const port = 8080;
+
 app.use(express.static('public'))
 
 app.get('/', function(req, res) {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 
-app.listen(3000, function() {
-  console.log('app is listening on port 3000');
+app.listen(port, function() {
+  console.log('app is listening on port ', port);
 })
